e310d2919a2e1738c6c067bfa1cf6f0aa6edfddc
Jack Madden <jck13mad@gmail.com>
2020-02-02 14:53:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   
   name_hash.each do |key, value|
-    if value
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value
+      
   end
 end
